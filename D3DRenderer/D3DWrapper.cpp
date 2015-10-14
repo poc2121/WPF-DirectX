07120059c68564bc34dc9d3f1246dabedd8432a1
poc2121 <poc2121@gmail.com>
2015-10-14 13:56:18 +0900

    refs #7 D3DWrapper 実装

--- D3DRenderer/D3DWrapper.cpp
+++ D3DRenderer/D3DWrapper.cpp
@@ -1,1 +1,31 @@
-﻿
+﻿#include "D3DRenderer.h" 
+#include <vcclr.h>
+using namespace System;
+namespace D3DMesh 
+{ 
+  public ref class D3DWrapper 
+  { 
+  public: 
+    IntPtr Initialize(IntPtr hwnd) 
+    { 
+      if (SUCCEEDED(::InitD3D((HWND) hwnd.ToPointer()))) 
+        return IntPtr(g_pd3dSurface); 
+      return IntPtr::Zero; 
+    } 
+
+    VOID InitGeometry() 
+    { 
+      ::InitGeometry(); 
+    }
+    VOID Render(int% width, int% height) 
+    { 
+      ::Render(); 
+      width = SCREEN_WIDTH; 
+      height = SCREEN_HEIGHT; 
+    }
+    VOID Cleanup() 
+    { 
+      ::Cleanup(); 
+    } 
+  };
+}
